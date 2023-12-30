1ecce9e4cf90f0586b4638a376f7972f442d1b32
Tramaine Jackson <jackson.tramaine3@gmail.com>
2023-12-30 10:50:38 -0500

    First Commit with Laravel 8

---
+++ webpack.mix.js
@@ -0,0 +1,21 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.sass('resources/src/scss/mdb.pro.scss', 'public/css/style.min.css');
+mix.postCss('resources/css/custom_styles.css', 'public/css/custom_styles.min.css');
+mix.postCss('resources/css/md-form.css', 'public/css/md-form.min.css');
+
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
